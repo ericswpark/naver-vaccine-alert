83ed489e940c92fe605bf9c3dbb03e70e0d39a71
Eric Park <me@ericswpark.com>
2021-07-06 16:30:51 +0900

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Check if venv directory exists
+if [ ! -d "venv" ]
+then
+    # Directory does not exist, initialize virtualenv
+    virtualenv venv
+    . venv/bin/activate
+
+    # Install dependencies
+    pip3 install -r requirements.txt
+
+    python main.py
+else
+    # Directory exists, run immediately
+    . venv/bin/activate
+    python main.py
+fi
