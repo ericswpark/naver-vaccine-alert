36c07a326e72b120b906692026754cc45529e8d1
Eric Park <me@ericswpark.com>
2021-07-06 23:15:06 +0900

    main: forgot casting to int

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ def adjust_time_delay(adj):
 def set_time_delay():
     global time_delay
 
-    time_delay = input("Enter new time delay in seconds (integers only): ")
+    time_delay = int(input("Enter new time delay in seconds (integers only): "))
     logger.info("New time delay: {}".format(time_delay))
 
 
