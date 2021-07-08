dae4a9d47c11c9a3a82201dffe9dfa71a70969cf
Eric Park <me@ericswpark.com>
2021-07-08 12:01:51 +0900

    Add location list finder

---
+++ get-location-list.py
@@ -0,0 +1,48 @@
+import json
+import argparse
+
+parser = argparse.ArgumentParser(description='Process response.json from main.py')
+parser.add_argument('-f', '--find', help='Find hospital from list')
+
+hospitals = []
+
+
+def parse_response_data():
+    with open('response.json', 'r') as file:
+        data = json.load(file)
+
+    locations = data[0]['data']['rests']['businesses']['items']
+
+    for location in locations:
+        location_name = location['name']
+        vaccine_count = location['vaccineQuantity']['totalQuantity']
+        hospitals.append({'name': location_name, 'count': vaccine_count})
+
+
+def print_location_data():
+    parse_response_data()
+    for location in hospitals:
+        print("{} - {} vaccines".format(location['name'], location['count']))
+
+
+def find_location(name):
+    parse_response_data()
+    for location in hospitals:
+        if name in location['name']:
+            print("Found location {}. Vaccine count: {}".format(location['name'], location['count']))
+            return
+
+    print("No location found.")
+    return
+
+
+def main():
+    args = parser.parse_args()
+    if args.find:
+        find_location(args.find)
+    else:
+        print_location_data()
+
+
+if __name__ == "__main__":
+    main()
